e6ddf892187f7db582778a3c2e9e1edceb519d07
Omar Khairy <omarkhairy21@gmail.com>
2019-01-28 03:34:53 +0200

    Intial commit

---
+++ routes/shop.js
@@ -0,0 +1,17 @@
+const path = require('path')
+
+const express = require('express');
+
+const rootDir = require('../util/path');
+
+const {products} = require('./admin');
+
+const router = express.Router();
+
+
+router.get('/',(req, res, next) =>{
+    console.log('shop.js', products);    
+    res.sendFile(path.join(rootDir,'views', 'shop.html'));
+})
+
+module.exports = router;
